3a051f9b77141584e7e204a0d5a180b732cc33f9
RuslanMak <makarovemd@gmail.com>
2019-01-16 21:15:44 +0200

    Create admin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,49 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="jumbotron">
+          <p><span class="label label-primary">Категорий 0</span></p>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="jumbotron">
+          <p><span class="label label-primary">Материалов 0</span></p>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="jumbotron">
+          <p><span class="label label-primary">Посетителей 0</span></p>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="jumbotron">
+          <p><span class="label label-primary">Сегодня 0</span></p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6">
+          <a class="bnt btn-block btn-default" href="#">Создать категорию</a>
+          <a class="list-group-item" href="#"></a>
+          <h4 class="list-group-item-heading">Категория первая</h4>
+          <p class="list-group-item-text">
+            Кол-во материалов
+          </p>
+        </div>
+
+        <div class="col-sm-6">
+          <a class="bnt btn-block btn-default" href="#">Создать материал</a>
+          <a class="list-group-item" href="#"></a>
+          <h4 class="list-group-item-heading">Материал первый</h4>
+          <p class="list-group-item-text">
+            Категория
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+@endsection
